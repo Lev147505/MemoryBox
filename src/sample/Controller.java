6aa01eee185b3f5ebade8f1ff668c37eedfe8cf1
Lev <lev-titov@mail.ru>
2018-06-06 17:03:59 +0300

    В контроллере создал элементы GUI с заданным fxId Экземпляр класса Client и передал туда ссылку на контроллер через конструктор написано 2 класса logIn и reg для тега onAction на кнопках GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sample.client.Client;
+
+
+public class Controller implements ViewControl{
+    @FXML private TextField loginField;
+    @FXML private PasswordField passField;
+    @FXML private TextField nick;
+    @FXML private TextArea textArea;
+
+    private Client client = new Client(this);
+
+    @FXML
+    public void logIn() {
+        if (loginField.getText().length() != 0 && passField.getText().length() != 0){
+            client.auth(loginField.getText(),passField.getText());
+        }
+        loginField.clear();
+        passField.clear();
+        nick.clear();
+    }
+
+    @FXML
+    public void register(){
+        if (loginField.getText().length() != 0 && passField.getText().length() != 0 && nick.getText().length() != 0){
+            client.reg(loginField.getText(),passField.getText(), nick.getText());
+        }
+    }
+
+    @FXML
+    public void writeInTextArea(String msg){
+        textArea.appendText(msg);
+    }
+
 }
